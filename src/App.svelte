616ad817ebf759568ba60b57af2cf59faba7c23a
Christopher Lam <127339027+clam365@users.noreply.github.com>
2025-06-17 17:56:36 -0400

    chatting with a friend!

--- src/App.svelte
+++ src/App.svelte
@@ -5,25 +5,40 @@
 
   // when connection is established...
   ws.onopen = () => {
-    ws.send( 'a new client has connected.' )
+    ws.send( 'A new user has joined the chat!' )
 
     ws.onmessage = async msg => {
       // add message to end of msgs array,
       // re-assign to trigger UI update
       const message = await msg.data.text()
-      msgs = msgs.concat([ 'them: ' + message ])
+      msgs = msgs.concat([message ])
     }
   }
 
   const send = function() {
-    const txt = document.querySelector('input').value
-    ws.send( txt )
-    msgs = msgs.concat([ 'me: ' + txt ])
+    const username = document.querySelector('#username').value
+    const txt = document.querySelector('#chatMessage').value;
+
+    const fullMessage = `${username}: ${txt}`;
+    ws.send( fullMessage )
+    msgs = msgs.concat([ `${username}: ` + txt ])
   }
 </script>
+<div>
+  <h2>Chat with a new friend!</h2>
+  <div style="display: block; text-align: left">
+    <label for="username">Enter your username:</label>
+    <input type="text" placeholder="Enter a username" id="username"/>
+  </div>
+</div>
+<div style="margin: auto; margin-top: 10px; border: 1px black solid; border-radius: 10px ;width: 400px; height: 400px; display: flex; flex-direction: column; padding: 5px">
+  <div style="flex: 1;overflow-y: auto;margin-bottom: 10px;">
+    {#each msgs as msg}
+      <div>{msg}</div>
+    {/each}
+  </div>
+  <div style="display: flex">
+    <input type='text' id="chatMessage" on:change={send} placeholder="Enter your message!" />
 
-<input type='text' on:change={send} />
-
-{#each msgs as msg }
-  <h3>{msg}</h3>
-{/each}
+  </div>
+</div>
